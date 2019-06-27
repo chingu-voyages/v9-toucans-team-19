6eb7c6e54888bc5218b7bd1707e24e8f0e74971c
Tim <Techne3@gmail.com>
2019-06-27 12:07:11 -0600

    file clean up

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,15 @@ function burgerDisplay() {
           x.style.display = "none";
       }
   }
+
+  //  function mediaQueriesBur(x) {
+//    let x= document.getElementById("burgerMenu")
+//    if (x.matches) { 
+//      document.burgerMenu.style.display="none";
+//    } else {
+//      document.burgerMenu.style.display="display";
+//    }
+//  }
+//  var x = window.matchMedia("(max-width: 1000px)")
+//  mediaQueriesBur();
+//  x.addListener(mediaQueriesBur)
