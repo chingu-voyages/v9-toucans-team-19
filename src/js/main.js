a9e0e149fbd26adf69d9c8562e439a75d663a218
Tim <Techne3@gmail.com>
2019-07-03 03:22:31 -0600

    icon animation

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,81 @@ function burgerDisplay() {
       }
   }
 
+
+  // TOGGLE modal section
+  let toggle  = document.getElementById("modal-circle");
+let content = document.getElementById("modal-card-section");
+
+toggle.addEventListener("click", function() {
+  content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+});
+
+
+// toggle icons on button
+
+$(document).ready(function(){
+  $('#modal-circle').click(function(){
+    $('span').toggleClass('active')
+  })
+})
+
+
+
+
+
+
+// $('button').click(function(){
+//   $(this).find('i').toggleClass('fas fa comment fas fa-times')
+// });
+
+// $('button').on('click', fav);
+
+// function fav(e) {
+//   $(this).find('i').toggleClass('fas fa-times fas fa-comment');
+// }
+
+
+
+
+
+
+//   let toggle  = document.getElementById("modal-circle2");
+// let content = document.getElementById("modal-card-section");
+
+// toggle.addEventListener("click", function() {
+//   content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+
+
+// 
+
+// function myFunction() {
+//   var element = document.getElementById("try");
+//   element.classList.toggle(" fa-times");
+// }
+// function fontToggle() {
+//   let x = document.getElementById("times");
+//   if(x.style.display=== "none"){
+//       x.style.display = "block";
+//   }else {
+//       x.style.display = "none";
+//   }
+// };
+// function fontToggle() {
+//   let x = document.getElementById("modal-circles2");
+//   if(x.style.display=== "none"){
+//       x.style.display = "block";
+//   }else {
+//       x.style.display = "none";
+//   }
+// };
+
+
+
+
+
+
+
+
   //  function mediaQueriesBur(x) {
 //    let x= document.getElementById("burgerMenu")
 //    if (x.matches) { 
@@ -29,3 +104,35 @@ function burgerDisplay() {
 //  var x = window.matchMedia("(max-width: 1000px)")
 //  mediaQueriesBur();
 //  x.addListener(mediaQueriesBur)
+
+// toggle the font awesome icons
+
+// $(document).ready(function() {
+//   $('.times').click(function() {
+//     $('i')
+//     .toggleClass('fas fa-comment')
+//     .toggleClass('fas fa-times');
+//   });
+// }); 
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   $('button').on('click', function () {
+//     $(this)
+//       .find('[data-fa-i2svg]')
+//       .toggleClass('fas fa-comment')
+//       .toggleClass('fas fa times');
+//   });
+// });
+// $('#modal-circle').click(function(){
+//   $(this).find('i').toggleClass('fas fa-times, fas fa-comment')
+  
+// });
+
+
+
+
+
